2345606d32e8207db9783d22e5ba11a1b3e6b6f4
Amira Anuar <amiraanuar@gmail.com>
2014-08-11 09:53:49 -0700

    Correctly check for horizontal four in a row

--- gameLogic.js
+++ gameLogic.js
@@ -50,8 +50,60 @@ Gameboard.prototype.dropCircle = function(column) {
   return false;
 };
 
-Gameboard.prototype.checkGame = function() {
+Gameboard.prototype.checkForWin = function(row,column) {
+  // Checking the next four from leftstart to rightend, topStart to bottomEnd
+  var board = this;
+  var leftStart = column-3 >= 0 ? column-3 : 0;
+  var rightEnd = this.columns-column >= 4 ? column : this.columns-4;
+  var topStart = row-3 >= 0 ? row-3 : 0;
+  var bottomEnd = this.rows-row >= 4 ? row : this.rows-4;
+  console.log(leftStart,rightEnd,topStart,bottomEnd);
 
+  var checkHorizontal = function() {
+    for (var i = leftStart; i <= rightEnd; i++) {
+      // For every i, we check the next 4
+      var rightFour = i+3<board.columns ? i+3 : board.columns-1;
+      var hasFourInARow = true;
+      for (var j = i; j <= rightFour; j++) {
+        if (board.boardMatrix[row][j] !== currentPlayer.color) {
+          hasFourInARow = false;
+        }
+      }
+      // If four in a row is still true after looping, return
+      if (hasFourInARow === true) {
+        return true;
+      }
+    }
+    return false;
+  };  
+
+  // check vertical
+  var checkVertical = function() {
+    for (var i = topStart; i <= bottomEnd; i++ ) {
+      var downFour = i+3 < board.rows ? i + 3 : board.rows-1;
+      var hasFourInARow = true;
+      for (var j = i; j <= downFour; j++ ) {
+        if (board.boardMatrix[j][column] !== currentPlayer.color) {
+          hasFourInARow = false;
+        }
+      }
+      if (hasFourInARow === true) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  // check diagonals
+  var checkDiagonal = function() {
+    return false;
+  };
+
+  console.log('vert', checkVertical()); 
+  if (checkHorizontal() || checkVertical() || checkDiagonal()) {
+    return true;
+  }
+  return false;
 };
 
 // Instantiate a new game board every page load, as well as two players
@@ -68,7 +120,9 @@ $('.circle').on('click', function(event) {
   var column = parseInt($(this).data('column'));
 	if(currentGame.dropCircle(column)) {
     // Check the game to see if there is a win
-
+    if(currentGame.checkForWin(row,column)) {
+      alert('You have won!');
+    }
 
     // We are now switching players
     currentPlayer = currentPlayer === playerOne ? playerTwo: playerOne;
